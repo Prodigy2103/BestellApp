01b44957744200d5ff4d634e6550f7e494e885ab
Prodigy2103 <marcusghne@gmx.de>
2025-07-24 14:30:21 +0200

    changes in index.html database.js, removed menuItemsArray from script.js and paste in databse.js and css changes

--- script.js
+++ script.js
@@ -43,56 +43,7 @@ class CardItem {
 
 let orderArray = [];
 
-let menuItemsArray = [
-    {
-        title: "Döner",
-        img: "img/CartImg/food-photo-8683717_640.jpg",
-        desc: "Schmeckt lecker mit viel Soße!",
-        price: 6.5,
-    },
-    {
-        title: "Döner Vegetarisch",
-        img: "img/CartImg/vegetarisch.jpg",
-        desc: "Lecker, fleischlos, frisch, sättigend, vielfältig.",
-        price: 5.0,
-    },
-    {
-        title: "Dürüm",
-        img: "img/CartImg/Dürüm.jpg",
-        desc: "Fladenbrot, gerollt, gefüllt, praktisch, lecker..",
-        price: 7.0,
-    },
-    {
-        title: "Lahmacun",
-        img: "img/CartImg/lahmacun_selbstgemacht-7591.jpg",
-        desc: "Knusprig, dünn, würzig, orientalisch, Fladenbrot.",
-        price: 7.0,
-    },
-    {
-        title: "Kumpir",
-        img: "img/CartImg/Kumpir-1.jpg",
-        desc: "Große Ofenkartoffel, vielfältig, cremig, sättigend, Beläge.",
-        price: 7.0,
-    },
-    {
-        title: "Falafel Teller",
-        img: "img/CartImg/5460-bunter-falafel-teller.jpg",
-        desc: "Knusprige Bällchen, Salat, Soßen, vegetarisch, komplett.",
-        price: 9.5,
-    },
-    {
-        title: "Pide",
-        img: "img/CartImg/pide-adobe-stock-bbvirys-277534955.jpg",
-        desc: "Türkisches Fladenbrot, gefüllt, herzhaft, ofenfrisch.",
-        price: 8.0,
-    },
-    {
-        title: "Taco (groß)",
-        img: "img/CartImg/db9c492e-8cf9-40f6-8318-3326244e9f4c.webp",
-        desc: "Fusion, würziges Fleisch, Tortilla, innovativ, lecker.",
-        price: 8.5,
-    },
-];
+
 
 function formatPrice(price) {
     let formattedPrice = price.toFixed(2);
